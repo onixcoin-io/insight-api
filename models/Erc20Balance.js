9dec7361d8878c1e74ecb9384ae9f4c7f39cad81
Sharath11 <sharu.amc@gmail.com>
2022-01-17 12:09:29 +0800

    Initial commit

---
+++ models/Erc20Balance.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const erc20BalanceSchema = new mongoose.Schema({
+    contract_address_base: {
+        type: String,
+        required: true
+    },
+    contract_address: {
+        type: String,
+        required: true
+    },
+    address_eth: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    amount: {
+        type: String
+    },
+    amount_hex: {
+        type: String
+    }
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+});
+
+erc20BalanceSchema.index({ contract_address: 1, address: 1}, { unique: true });
+
+const Erc20Balance = mongoose.model('Erc20Balance', erc20BalanceSchema);
+
+module.exports = Erc20Balance;
